8992b5065e76c17325c964cbd42458f86879e6fa
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-09-01 10:52:57 -0400

    api key, mapeo de la respuesta, interface y axios

--- gifsApp/src/GifsApp.tsx
+++ gifsApp/src/GifsApp.tsx
@@ -4,6 +4,7 @@ import { PreviousSearches } from "./gifs/components/PreviousSearches";
 import { mockGifs } from "./mock-data/gifs.mock";
 import { CustomHeader } from "./shared/components/CustomHeader";
 import { SearchBar } from "./shared/components/SearchBar";
+import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action";
 
 export default function GifsApp() {
   const [previousTerms, setPreviousTerms] = useState(["goku", "dragon ball z"]);
@@ -12,12 +13,15 @@ export default function GifsApp() {
     console.log(`Término buscado: ${term}`);
   };
 
-  const handleSearch = (query: string) => {
+  const handleSearch = async(query: string) => {
     query = query.trim().toLowerCase();
     if (query.length === 0) return;
     if (previousTerms.includes(query)) return;
     
     setPreviousTerms([query, ...previousTerms].slice(0, 7));
+
+    const gifs = await getGifsByQuery(query);
+    console.log({gifs});
   };
 
   return (
